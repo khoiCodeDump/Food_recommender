00975fb5e0bada7cd4f8bd7a49434449f2141328
khoiCodeDump <khoivu617@gmail.com>
2024-10-19 02:09:20 -0700

    Minor fix on search

--- website/models.py
+++ website/models.py
@@ -328,7 +328,7 @@ def tfidf_search_recipes(query, top_n=100):
     # Return the top matching recipes
     return recipes
 
-def combined_search_recipes(user_query, k_elements=100, semantic_threshold=0.1, tfidf_threshold=0.2, tfidf_boost=1.5):
+def combined_search_recipes(user_query, k_elements=100, semantic_threshold=0.1, tfidf_threshold=0.05, tfidf_boost=1):
     global faiss_index, model, vectorizer, tfidf_matrix
 
     # Semantic Search
